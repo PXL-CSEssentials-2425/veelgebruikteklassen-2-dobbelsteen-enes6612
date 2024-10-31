0a997dd86b406b478a35631408a5e810d3c3e8c2
Enes Tanriverdi <Enes.Tanriverdi@student.pxl.be>
2024-10-31 14:54:32 +0100

    gemaakt

---
+++ dobbelsteen/dobbelsteen/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dobbelsteen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 7);
+            int worp = 0;
+            resultTextBox.Text = "";
+
+            do
+            {
+                randomNumber = random.Next(1, 7);
+                worp++;
+                resultTextBox.AppendText($"Worp {worp} geeft {randomNumber}\r\n");
+
+            } while (randomNumber != 6);
+        }
+
+        private void againButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+            startButton.Focus();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
